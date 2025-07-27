5a12c5d621b5846e9dbf1c64486044edacd3a2e2
tmcgahey93 <tmcgahey93@gmail.com>
2025-07-27 16:19:10 -0400

    Initial Commit

---
+++ src/main/java/com/example/java_transaction_generator/JavaTransactionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_transaction_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTransactionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTransactionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_transaction_generator/JavaTransactionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_transaction_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTransactionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
