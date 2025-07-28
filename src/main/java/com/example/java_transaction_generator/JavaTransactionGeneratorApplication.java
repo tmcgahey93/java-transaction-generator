4f1391fa53435b23266404f3fadf9b10541ed1e1
tmcgahey93 <tmcgahey93@gmail.com>
2025-07-27 20:53:47 -0400

    Add kafka producer

--- src/main/java/com/example/java_transaction_generator/JavaTransactionGeneratorApplication.java
+++ src/main/java/com/example/java_transaction_generator/JavaTransactionGeneratorApplication.java
@@ -10,14 +10,14 @@ public class JavaTransactionGeneratorApplication {
 
 	public static void main(String[] args) {
 
-		//String topic = "transactions";
+		String topic = "transactions";
 
-		//Properties props = new Properties();
-		//props.put("bootstrap.servers", "localhost:9092");
-		//props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		//props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		//KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+		KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 		ObjectMapper mapper = new ObjectMapper();
 
 		while(true) {
@@ -26,8 +26,8 @@ public class JavaTransactionGeneratorApplication {
 
 			try {
             	String json = mapper.writeValueAsString(txn);
-				//ProducerRecord<String, String> record = new ProducerRecord<>(topic, txn.userId, json);
-				//producer.send(record);
+				ProducerRecord<String, String> record = new ProducerRecord<>(topic, txn.userId, json);
+				producer.send(record);
 				System.out.println("Sent: " + json);
 	
 				Thread.sleep(1000);
